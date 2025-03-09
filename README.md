91f29d0895f51286624f9f530b1bcb0683b3460b
SomeRandomDeveloper <thisisnotmyname275@gmail.com>
2025-03-09 20:50:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# IRWA Templates
+
+This repo provides and deploys shared templates on member wikis of the Independent Roblox Wiki Alliance.
+
+## Setup for Miraheze wikis
+
+(TODO)
+
+## Setup for 3rd-party-wikis
+
+Requirements:
+* https://www.mediawiki.org/wiki/Extension:OAuth
+
+(TODO)
+
+### Bot permissions
+
+![A screenshot of Special:OAuthConsumerRegistration showing the rights required by the bot.](assets/img/bot_rights.png)
+
+* Edit existing pages
+* Create, edit and move pages
---
+++ bot.ts
@@ -0,0 +1,58 @@
+import { Mwn } from "mwn";
+import * as dotenv from "dotenv";
+import * as fs from "fs";
+
+dotenv.config();
+
+const commitMessage = process.env.COMMIT_MESSAGE ?? "Update template";
+const commitAuthorName = process.env.COMMIT_AUTHOR_NAME;
+
+type Wiki = {
+  apiUrl: string;
+  accessToken: string;
+};
+
+type Template = {
+  filePath: string;
+  pageName: string;
+};
+
+const wikis: Wiki[] = require("./data/wikis.json");
+const templates: Template[] = require("./data/templates.json");
+
+async function updateTemplateOnWiki(wiki: Wiki) {
+  const accessToken = process.env[wiki.accessToken];
+
+  if (!accessToken) {
+    console.warn("Failed to find access token for wiki " + wiki.apiUrl + "!");
+    return;
+  }
+
+  const bot = await Mwn.init({
+    apiUrl: wiki.apiUrl,
+    OAuth2AccessToken: accessToken,
+    userAgent:
+      "RobloxWikiAllianceBot/1.0 (https://github.com/Roblox-Indie-Wikis/Templates)",
+    defaultParams: {
+      assert: "user",
+    },
+  });
+
+  templates.forEach(async (template: Template) => {
+    const content = fs.readFileSync(template.filePath, "utf-8");
+
+    let editSummary = commitMessage;
+    if (commitAuthorName) {
+      editSummary += " (" + commitAuthorName + ")";
+    }
+
+    console.log(
+      "Updating " + template.pageName + " on wiki " + wiki.apiUrl + "..."
+    );
+    await bot.save(template.pageName, content, editSummary);
+  });
+}
+
+wikis.forEach(async (wiki: Wiki) => {
+  updateTemplateOnWiki(wiki);
+});
